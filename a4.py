0cc3c3093d06f7e06493f4ebd9feb61de926e51c
Eliana Nieves <EMNieves@cps.edu>
2024-11-11 11:25:07 -0600

    finished tic tac toe

--- a4.py
+++ a4.py
@@ -10,50 +10,82 @@ class TTTBoard:
             represent moves by player 'O' and '*'s are spots no one has yet played on
     """
 
-    pass
+    def __init__(self) -> None:
+        self.board = ['*'] * 9
+
+    def __str__(self) -> str:
+        s = ""
+        for x in [0, 3, 6]:
+            s += self.board[x + 0] + " " + self.board[x + 1] + " " + self.board[x + 2] + "\n"
+        return s
+
+    def make_move(self, player, pos) -> bool:
+        if pos < 0 or pos > 8 or self.board[pos] != '*':
+            return False
+
+        self.board[pos] = player
+        return True
+
+    def has_won(self, player) -> bool:
+        ps = [player] * 3
+        if self.board[:3] == ps or self.board[3:6] == ps or self.board[6:] == ps:
+            return True
+        if self.board[::3] == ps or self.board[1::3] == ps or self.board[2::3] == ps:
+            return True
+        if self.board[::4] == ps or self.board[2:7:2] == ps:
+            return True
+        return False
+
+    def game_over(self) -> bool:
+        if self.has_won("X") or self.has_won("O") or "*" not in self.board:
+            return True
+        return False
+
+    def clear(self) -> None:
+        self.board = ["*"] * 9
 
 
 def play_tic_tac_toe() -> None:
     """Uses your class to play TicTacToe"""
 
-    def is_int(maybe_int: str):
-        """Returns True if val is int, False otherwise
+def is_int(maybe_int: str):
+    """Returns True if val is int, False otherwise
 
-        Args:
-            maybe_int - string to check if it's an int
+     Args:
+        maybe_int - string to check if it's an int
 
-        Returns:
-            True if maybe_int is an int, False otherwise
-        """
-        try:
-            int(maybe_int)
-            return True
-        except ValueError:
-            return False
+     Returns:
+        True if maybe_int is an int, False otherwise
+    """
+    try:
+        int(maybe_int)
+        return True
+    except ValueError:
+        return False
 
-    brd = TTTBoard()
-    players = ["X", "O"]
-    turn = 0
+brd = TTTBoard()
+players = ["X", "O"]
+turn = 0
 
-    while not brd.game_over():
-        print(brd)
-        move: str = input(f"Player {players[turn]} what is your move? ")
+while not brd.game_over():
+    print(brd)
+    move: str = input(f"Player {players[turn]} what is your move? ")
 
-        if not is_int(move):
-            raise ValueError(
-                f"Given invalid position {move}, position must be integer between 0 and 8 inclusive"
-            )
+    if not is_int(move):
+        raise ValueError(
+            f"Given invalid position {move}, position must be integer between 0 and 8 inclusive"
+        )
 
-        if brd.make_move(players[turn], int(move)):
-            turn = not turn
+    if brd.make_move(players[turn], int(move)):
+        turn = not turn
 
-    print(f"\nGame over!\n\n{brd}")
-    if brd.has_won(players[0]):
-        print(f"{players[0]} wins!")
-    elif brd.has_won(players[1]):
-        print(f"{players[1]} wins!")
-    else:
-        print(f"Board full, cat's game!")
+print(f"\nGame over!\n\n{brd}")
+if brd.has_won(players[0]):
+    print(f"{players[0]} wins!")
+elif brd.has_won(players[1]):
+    print(f"{players[1]} wins!")
+else:
+    print(f"Board full, cat's game!")
 
 
 if __name__ == "__main__":
@@ -61,8 +93,12 @@ if __name__ == "__main__":
     # need to write some more tests to make sure that your TTTBoard class is behaving
     # properly.
     brd = TTTBoard()
+    print(brd)
     brd.make_move("X", 8)
+    brd.make_move("O", 8)
     brd.make_move("O", 7)
+
+    print(brd)
 
     assert brd.game_over() == False
 
@@ -89,4 +125,4 @@ if __name__ == "__main__":
     print("All tests passed!")
 
     # uncomment to play!
-    # play_tic_tac_toe()
+play_tic_tac_toe()
